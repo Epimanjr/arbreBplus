7adc465a941ad8e9b90df46a8fe2639a0e168490
Maxime <Maxime@Yirumax-PC>
2014-10-19 18:07:15 +0200

    ...

--- src/tests/PrincipaleTest.java
+++ src/tests/PrincipaleTest.java
@@ -17,16 +17,16 @@ public class PrincipaleTest {
     public static void main(String[] args) {
         // Création d'un arbre
         Arbre b = new Arbre(3, "5", "toto");
-        b.insertion("6", "antoine");
-        b.insertion("4", "maxime");
-        b.insertion("7", "Denniss");
-        b.insertion("2", "geoffrey");
-        b.insertion("8", "monsieur");
-        b.insertion("1", "UN");
-        b.insertion("3", "TROIS");
-        b.insertion("9", "NEUF");
-        b.insertion("10", "DIX");
-        b.insertion("11", "ONZE");
+        b.insertion("20", "antoine");
+        b.insertion("10", "maxime");
+        //b.insertion("40", "Denniss");
+        //b.insertion("30", "geoffrey");
+        //b.insertion("25", "monsieur");
+        //b.insertion("35", "UN");
+        //b.insertion("15", "TROIS");
+       // b.insertion("80", "NEUF");
+        //b.insertion("90", "DIX");
+        //b.insertion("55", "ONZE");
 
        /* System.out.println("\nTaille de la racine : " + b.getRacine().getTabCle().size());
 
