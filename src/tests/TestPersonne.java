acf7a21454633cc53ea55c483c46bd4050a3189c
Maxime <Maxime@Yirumax-PC>
2014-10-24 09:11:53 +0200

    Commit des fichiers

--- src/tests/TestPersonne.java
+++ src/tests/TestPersonne.java
@@ -17,6 +17,12 @@ import java.util.Scanner;
  */
 public class TestPersonne {
 
+    /**
+     * Arbre courant.
+     */
+    public static Arbre[] arbreUtilise = new Arbre[2];
+    public static int indice;
+    
     public static void main(String[] args) {
 
         //Parcourir tous les fichiers      
@@ -25,7 +31,7 @@ public class TestPersonne {
         //Créer l'arbre de personnes
         String chemin;
         ArrayList<Personne> liste = new ArrayList<>();
-
+        
         for (int i = 1; i < 51; i++) {
             // Il faut mettre le dossier des données dans C:/DONNEES
             // Ex : C:/DONNEES/F1.txt
@@ -33,19 +39,20 @@ public class TestPersonne {
             liste.add(Personne.chargerDepuisFichier(chemin));
             System.out.println("Importation de " + liste.get(i - 1) + "reussi.");
         }
-
+        
         System.out.println("");
 
-        /*// Création d'un arbre
-         Arbre b = new Arbre(3, liste.get(0).getAge() + "", liste.get(0).getNom());
-         // Insertions
-         for (int i = 1; i < 50; i++) {
-         b.insertion(liste.get(i).getAge() + "", liste.get(i).getNom());
-         }
-         System.out.println("Arbre final (Cle=Age, Valeur=Prenom): ");
+        // Création d'un arbre
+        Arbre b = new Arbre(3, liste.get(0).getAge() + "", liste.get(0).getNom());
+        // Insertions
+        for (int i = 1; i < 50; i++) {
+            b.insertion(liste.get(i).getAge() + "", liste.get(i).getNom());
+        }
+        /*System.out.println("Arbre final (Cle=Age, Valeur=Prenom): ");
          // Affichage de l'arbre
          AffichageConsole.afficherArbre(b);
          System.out.println("\n\n\n");*/
+
         // Création d'un arbre
         Arbre b2 = new Arbre(3, liste.get(0).getNom(), liste.get(0).getAge() + "");
         // Insertions
@@ -55,15 +62,19 @@ public class TestPersonne {
         System.out.println("Arbre final (Cle=Prenom, Valeur=Age): ");
         // Affichage de l'arbre
         AffichageConsole.afficherArbre(b2);
+        
+        arbreUtilise[0] = b;
+        arbreUtilise[1] = b2;
+        indice = 1;
 
         // Lancement d'un SHELL
         Scanner sc = new Scanner(System.in);
-
+        
         afficheAide();
         while (true) {
             System.out.print(">>> ");
             String cmd = sc.nextLine();
-
+            
             String[] split = cmd.split(" ");
             switch (split.length) {
                 case 1:
@@ -71,40 +82,62 @@ public class TestPersonne {
                         case "help":
                             afficheAide();
                             break;
+                        case "switch":
+                            // Changement de l'indice
+                            if (indice == 0) {
+                                indice = 1;
+                            } else {
+                                indice = 0;
+                            }
+
+                            // Affichage de l'arbre
+                            AffichageConsole.afficherArbre(arbreUtilise[indice]);
+                            break;
                         case "exit":
                             System.out.println("*** FIN DU PROGRAMME ***");
                             System.exit(1);
                             break;
                         case "show":
                             // Affichage de l'arbre
-                            AffichageConsole.afficherArbre(b2);
+                            System.out.println("Affichage de l'arbre ... ");
+                            AffichageConsole.afficherArbre(arbreUtilise[indice]);
                             break;
                     }
                     break;
                 case 2:
                     if (split[0].equals("get")) {
                         // On cherche la valeur pour l'afficher
-                        System.out.println(b2.chercherValeur(split[1]));
+                        System.out.println(arbreUtilise[indice].chercherValeur(split[1]));
+                    }
+                    if (split[0].equals("sup")) {
+                        // On supprime une cle
+                        System.out.print("Suppression de " + split[1] + " ... ");
+                        arbreUtilise[indice].suppression(split[1]);
+                        System.out.println("Fait.");
                     }
                     break;
                 case 3:
                     if (split[0].equals("add")) {
                         // On insère une nouvelle valeur dans l'arbre
-                        b2.insertion(split[1], split[2]);
+                        System.out.print("Ajout de ("+split[1]+","+split[2]+") ... ");
+                        arbreUtilise[indice].insertion(split[1], split[2]);
+                        System.out.println("Fait.");
                     }
                     break;
             }
         }
     }
-
+    
     private static void afficheAide() {
         System.out.println("\n\tEntrer une commande : ");
         System.out.println("\t------------------------");
         System.out.println("help               : affiche la presente aide.");
         System.out.println("show               : affiche l'arbre.");
+        System.out.println("switch             : changer le type d'arbre (inversion cle/valeur)");
         System.out.println("add <cle> <valeur> : ajoute une donnee dans l'arbre.");
         System.out.println("get <cle>          : cherche une valeur dans l'arbre.");
+        System.out.println("sup <cle>          : supprime une cle dans l'arbre (version alpha)");
         System.out.println("exit               : quitte le programme.");
     }
-
+    
 }
